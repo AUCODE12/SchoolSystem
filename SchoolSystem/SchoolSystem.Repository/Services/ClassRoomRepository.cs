facaaaffa52f33654a3800c73a687045bb0f2cef
Ahmadjon Qudratov <qahmadjon11@gmail.com>
2025-04-10 17:30:05 +0500

    ok

---
+++ SchoolSystem/SchoolSystem.Repository/Services/ClassRoomRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolSystem.Dal;
+using SchoolSystem.Dal.Entities;
+
+namespace SchoolSystem.Repository.Services;
+
+public class ClassRoomRepository : IClassRoomRepository
+{
+    private readonly MainContext _context;
+
+    public ClassRoomRepository(MainContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<ClassRoom> GetByIdAsync(int id) => await _context.ClassRooms.FindAsync(id);
+
+    public async Task<List<ClassRoom>> GetAllAsync() => await _context.ClassRooms.ToListAsync();
+
+    public async Task AddAsync(ClassRoom classRoom)
+    {
+        await _context.ClassRooms.AddAsync(classRoom);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(ClassRoom classRoom)
+    {
+        _context.ClassRooms.Update(classRoom);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(ClassRoom classRoom)
+    {
+        _context.ClassRooms.Remove(classRoom);
+        await _context.SaveChangesAsync();
+    }
+}
+
---
+++ SchoolSystem/SchoolSystem.Repository/Services/IStudentRepository.cs
@@ -0,0 +1,12 @@
+﻿using SchoolSystem.Dal.Entities;
+
+namespace SchoolSystem.Repository.Services;
+
+public interface IStudentRepository
+{
+    Task<Student> GetByIdAsync(int id);
+    Task<List<Student>> GetAllAsync();
+    Task AddAsync(Student student);
+    Task UpdateAsync(Student student);
+    Task DeleteAsync(Student student);
+}
