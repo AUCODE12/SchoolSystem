a52fd4ae26d18b532f18d155d149f1e9b7f86390
Ahmadjon Qudratov <qahmadjon11@gmail.com>
2025-04-10 17:42:54 +0500

    ok

--- SchoolSystem/SchoolSystem.Dal/MainContext.cs
+++ SchoolSystem/SchoolSystem.Dal/MainContext.cs
@@ -1,31 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using SchoolSystem.Dal.Entities;
-using SchoolSystem.Dal.EntityConfiguration;
 
 namespace SchoolSystem.Dal;
 
 public class MainContext : DbContext
 {
-    public DbSet<ClassRoom> ClassRooms { get; set; }
-    public DbSet<Student> Students { get; set; }
-    public DbSet<Teacher> Teachers { get; set; }
-    public DbSet<ClassRoomStudent> ClassRoomStudents { get; set; }
-    public DbSet<ClassRoomTeacher> ClassRoomTeachers { get; set; }
-    public DbSet<TeacherStudent> TeacherStudents { get; set; }
-
     public MainContext(DbContextOptions options) : base(options)
     {
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.ApplyConfiguration(new ClassRoomConfiguration());
-        modelBuilder.ApplyConfiguration(new StudentConfiguration());
-        modelBuilder.ApplyConfiguration(new TeacherConfiguration());
-        modelBuilder.ApplyConfiguration(new ClassRoomStudentConfiguration());
-        modelBuilder.ApplyConfiguration(new ClassRoomTeacherConfiguration());
-        modelBuilder.ApplyConfiguration(new TeacherStudentConfiguration());
-
         base.OnModelCreating(modelBuilder);
     }
 
